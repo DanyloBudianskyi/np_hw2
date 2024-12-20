d475a1aef70cdf8f112c77745f14dab902a95d84
Danil <super.danil501@gmail.com>
2024-12-19 21:54:28 -0700

    Add project files.

---
+++ np_hw2_client/Program.cs
@@ -0,0 +1,146 @@
+﻿using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace ls1_client
+{
+    public class Client
+    {
+        private static UdpClient client { get; set; } = new UdpClient();
+        private static IPEndPoint serverEndpoint { get; set; } = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 5000);
+        private static string Username { get; set; }
+        private static string Password { get; set; }
+
+        static async Task Main(string[] args)
+        {
+            Console.Write("Enter your username: ");
+            Username = Console.ReadLine();
+            Console.Write("Enter your password: ");
+            Password = Console.ReadLine();
+
+            string userInfo = $"{Username} {Password}";
+            byte[] buffer = Encoding.UTF8.GetBytes(userInfo);
+            await client.SendAsync(buffer, buffer.Length, serverEndpoint);
+
+            Task receiveTask = ReceiveMessageAsync();
+            Console.Write("Connected to server.\n/quit - to exit.\n/pm username and your message - for private message.\n/file username filepath - send file to user \n/history to check your message history\nEnter your first message: ");
+            while (true)
+            {
+                try
+                {
+                    string message = Console.ReadLine();
+                    buffer = Encoding.UTF8.GetBytes(message);
+                    await client.SendAsync(buffer, buffer.Length, serverEndpoint);
+
+                    if (message == "/quit")
+                    {
+                        break;
+                    }
+                    else if (message.StartsWith("/file"))
+                    {
+                        string[] command = message.Split(' ', 3);
+                        if (command.Length >= 3)
+                        {
+                            string targetUser = command[1];
+                            string path = command[2];
+                            if (File.Exists(path))
+                            {
+                                string fileName = Path.GetFileName(path);
+                                byte[] msgBuf = Encoding.UTF8.GetBytes($"/file {targetUser} {fileName}");
+                                await client.SendAsync(msgBuf, msgBuf.Length, serverEndpoint);
+                                SendFile(path);
+                            }
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+            client.Close();
+        }
+
+        static async Task ReceiveMessageAsync()
+        {
+            try
+            {
+                while (true)
+                {
+                    UdpReceiveResult result = await client.ReceiveAsync();
+                    string received = Encoding.UTF8.GetString(result.Buffer, 0, result.Buffer.Length);
+                    if (received.StartsWith("file:"))
+                    {
+                        string fileName = received.Substring(5).Trim();
+                        ReceiveFile(fileName, 5002);
+                    }
+                    else
+                    {
+                        Console.WriteLine(received);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        private static void ReceiveFile(string path, int port)
+        {
+            using (UdpClient client = new UdpClient(port))
+            {
+                IPEndPoint iPEndPoint = new IPEndPoint(IPAddress.Any, port);
+
+                byte[] buffer_size = client.Receive(ref iPEndPoint);
+                int file_size = BitConverter.ToInt32(buffer_size, 0);
+                Console.WriteLine($"Got size file: {file_size}");
+
+                byte[] file_buffer = new byte[file_size];
+                int receivedBytes = 0;
+                while (receivedBytes < file_size)
+                {
+                    byte[] part_buffer = client.Receive(ref iPEndPoint);
+                    Array.Copy(part_buffer, 0, file_buffer, receivedBytes, part_buffer.Length);
+                    receivedBytes += part_buffer.Length;
+                    Console.WriteLine($"Got: {receivedBytes} - {file_size}");
+                }
+
+                File.WriteAllBytes(path, file_buffer);
+                Console.WriteLine($"File saved successfully at {path}");
+            }
+        }
+
+
+
+
+
+
+
+        private static void SendFile(string path)
+        {
+            const int size_buffer = 8192;
+            using (UdpClient client = new UdpClient())
+            {
+                IPEndPoint serverEndPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 5001); // Порт сервера для приема файлов
+                byte[] file_buffer = File.ReadAllBytes(path);
+                Console.WriteLine($"Sending file of size: {file_buffer.Length} bytes");
+                client.Send(BitConverter.GetBytes(file_buffer.Length), 4, serverEndPoint); // Отправляем размер файла
+
+                for (int i = 0; i < file_buffer.Length; i += size_buffer)
+                {
+                    int part_size = Math.Min(size_buffer, file_buffer.Length - i);
+                    byte[] buffer_part = new byte[part_size];
+                    Array.Copy(file_buffer, i, buffer_part, 0, part_size);
+                    client.Send(buffer_part, part_size, serverEndPoint);
+                    Console.WriteLine($"Sent: {i + part_size} - {file_buffer.Length} bytes...");
+                }
+            }
+        }
+
+
+
+
+    }
+}
---
+++ np_hw2_server/Program.cs
@@ -0,0 +1,220 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace np_hw1_server
+{
+    public class ChatServer
+    {
+        public List<Client> Clients = new List<Client>();
+        private UdpClient Listener { get; set; }
+        private Dictionary<string, string> UserInfo = new Dictionary<string, string>() { { "User1", "password1" }, { "User2", "password2" } };
+        private int Port { get; set; }
+
+        public ChatServer(int port)
+        {
+            Port = port;
+            Listener = new UdpClient(port);
+        }
+
+        public async Task Listen()
+        {
+            Console.WriteLine("Server started and listening...");
+            while (true)
+            {
+                UdpReceiveResult result = await Listener.ReceiveAsync();
+                HandleClientAsync(result);
+            }
+        }
+
+        private async void HandleClientAsync(UdpReceiveResult result)
+        {
+            byte[] buffer = result.Buffer;
+            IPEndPoint clientEndPoint = result.RemoteEndPoint;
+            string message = Encoding.UTF8.GetString(buffer);
+
+            if (!Clients.Any(c => c.EndPoint.Equals(clientEndPoint)))
+            {
+                string[] userInfo = message.Split(' ');
+                if (userInfo.Length == 2 && AuthUser(userInfo[0], userInfo[1]))
+                {
+                    Clients.Add(new Client { EndPoint = clientEndPoint, Username = userInfo[0] });
+                    Console.WriteLine($"{userInfo[0]} has joined");
+                }
+                else
+                {
+                    Console.WriteLine($"Authentication failed for user: {userInfo[0]}");
+                    return;
+                }
+            }
+
+            Client client = Clients.First(c => c.EndPoint.Equals(clientEndPoint));
+            if (message.StartsWith("/file"))
+            {
+                string[] fileMessage = message.Split(' ', 3);
+                if (fileMessage.Length == 3)
+                {
+                    string targetUser = fileMessage[1];
+                    string filePath = fileMessage[2];
+                    Console.WriteLine(filePath);
+                    ReceiveAndForwardFile(targetUser, clientEndPoint, filePath);
+                }
+            }
+            else if (message.StartsWith("/pm")) 
+            {
+                string[] pmMessage = message.Split(' ', 3); 
+                if (pmMessage.Length == 3) 
+                {
+                    string targetUser = pmMessage[1]; 
+                    string privateMessage = pmMessage[2]; 
+                    SendPrivateMessage(targetUser, $"{client.Username}: {privateMessage}", client.EndPoint); 
+                }
+            }
+            else if(message == "/history")
+            {
+                SendHistory(clientEndPoint, client.MessageHistory);
+            }
+            else
+            {
+                Console.WriteLine($"{client.Username}: {message}");
+                client.MessageHistory.Add(message);
+                BroadCastMessage($"{client.Username}: {message}", client.EndPoint);
+            }
+
+            if (message == "/quit")
+            {
+                Clients.Remove(client);
+                Console.WriteLine($"Client {clientEndPoint} has left the chat.");
+            }
+        }
+        private static byte[] ReceiveFile(int receivePort)
+        {
+            const int size_buffer = 8192;
+            byte[] file_buffer = null;
+
+            UdpClient server = null;
+            try
+            {
+                server = new UdpClient(receivePort);
+                IPEndPoint clientReceiveEndPoint = new IPEndPoint(IPAddress.Any, receivePort);
+
+                byte[] file_size_result = server.Receive(ref clientReceiveEndPoint);
+                int file_size = BitConverter.ToInt32(file_size_result, 0);
+
+                file_buffer = new byte[file_size];
+                int received_bytes = 0;
+                while (received_bytes < file_size)
+                {
+                    byte[] buffer_part = server.Receive(ref clientReceiveEndPoint);
+                    Array.Copy(buffer_part, 0, file_buffer, received_bytes, buffer_part.Length);
+                    received_bytes += buffer_part.Length;
+                }
+            }
+            finally
+            {
+                server?.Close();
+            }
+
+            return file_buffer;
+        }
+
+
+
+        private static void ForwardFile(byte[] file_buffer, string targetIp, int forwardPort)
+        {
+            const int size_buffer = 8192;
+
+            UdpClient forwardClient = null;
+            try
+            {
+                forwardClient = new UdpClient();
+                IPEndPoint forwardEndPoint = new IPEndPoint(IPAddress.Parse(targetIp), forwardPort);
+                forwardClient.Send(BitConverter.GetBytes(file_buffer.Length), 4, forwardEndPoint);
+                for (int i = 0; i < file_buffer.Length; i += size_buffer)
+                {
+                    int part_size = Math.Min(size_buffer, file_buffer.Length - i);
+                    byte[] buffer_part = new byte[part_size];
+                    Array.Copy(file_buffer, i, buffer_part, 0, part_size);
+                    forwardClient.Send(buffer_part, part_size, forwardEndPoint);
+                }
+            }
+            finally
+            {
+                forwardClient?.Close();
+            }
+        }
+
+
+
+        private void ReceiveAndForwardFile(string targetUser, IPEndPoint clientEndPoint, string filePath)
+        {
+            Client targetClient = Clients.FirstOrDefault(c => c.Username == targetUser);
+            if (targetClient != null)
+            {
+                string fileName = Path.GetFileName(filePath);
+                string notificationMessage = "file:" + fileName;
+                byte[] notificationBuffer = Encoding.UTF8.GetBytes(notificationMessage);
+                Listener.Send(notificationBuffer, notificationBuffer.Length, targetClient.EndPoint);
+
+                string targetIp = "127.0.0.1";
+                int receivePort = 5001;
+                int forwardPort = 5002;
+                byte[] file_buffer = ReceiveFile(receivePort);
+                Console.WriteLine("File received");
+                ForwardFile(file_buffer, targetIp, forwardPort);
+                Console.WriteLine("File forwarded");
+            }
+        }
+        private void SendPrivateMessage(string targetUser, string message, IPEndPoint senderEndPoint) 
+        {
+            Client targetClient = Clients.FirstOrDefault(c => c.Username == targetUser); 
+            if (targetClient != null) 
+            {
+                byte[] buffer = Encoding.UTF8.GetBytes(message); 
+                Listener.Send(buffer, buffer.Length, targetClient.EndPoint); 
+            } 
+        }
+        private void SendHistory(IPEndPoint clientEndPoint, List<string> messageHistory) 
+        {
+            foreach (var message in messageHistory) 
+            {
+                string historyMessage = "history:" + message; 
+                byte[] buffer = Encoding.UTF8.GetBytes(historyMessage); 
+                Listener.Send(buffer, buffer.Length, clientEndPoint); 
+            } 
+        }
+
+        public void BroadCastMessage(string message, IPEndPoint excludeClient)
+        {
+            byte[] buffer = Encoding.UTF8.GetBytes(message);
+            foreach (var c in Clients)
+            {
+                if (!c.EndPoint.Equals(excludeClient))
+                {
+                    Listener.Send(buffer, buffer.Length, c.EndPoint);
+                }
+            }
+        }
+
+        public bool AuthUser(string username, string password)
+        {
+            return UserInfo.TryGetValue(username, out var _password) && _password == password;
+        }
+    }
+
+    public class Client
+    {
+        public IPEndPoint EndPoint { get; set; }
+        public string Username { get; set; }
+        public List<string> MessageHistory { get; set; } = new List<string>();
+    }
+
+    public class Program
+    {
+        static async Task Main(string[] args)
+        {
+            ChatServer chatServer = new ChatServer(5000);
+            await chatServer.Listen();
+        }
+    }
+}
